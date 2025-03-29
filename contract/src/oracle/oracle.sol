0854a257dd60a065e15263a3bf199441a60bba16
Maa-ly <lydiaahenkorahgyamfi@gmail.com>
2025-03-29 02:08:18 -0700

    oracle factory started

---
+++ contract/src/oracle/oracle.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.28;
+
+contract Oracle {
+    //ToDO
+    // 1. Add Chainlink price feed
+    // 2. Add Chainlink Oracle
+    // 3. Add Chainlink Aggregator
+    // 4. Add Chainlink AggregatorV3Interface
+}
